ad4f5231eeb217d3e8ff8dfb9d3c08fb6baee98a
mahou <tyanmahou@gmail.com>
2019-02-11 01:21:09 +0900

    update light effect

--- System/BGLight.hpp
+++ System/BGLight.hpp
@@ -7,13 +7,15 @@ struct BGLight : IEffect
 	double m_maxRadius;
 	double m_speed;
 	double m_deflection;
+	int m_h;
 	Vec2 m_pos;
 
 	BGLight() :
 		m_maxRadius(Random(10.0, 150.0)),
 		m_speed(Random(-1.0, -5.0)),
 		m_deflection(Random(0.0, 100.0)),
-		m_pos(RandomVec2({ 0,300,800,600 }))
+		m_pos(RandomVec2({ 0,300,800,600 })),
+		m_h(System::FrameCount()%360)
 	{}
 
 	bool update(double t) override
@@ -22,7 +24,9 @@ struct BGLight : IEffect
 		const double r = Min(m_maxRadius, EaseOut(0.0, m_maxRadius, Easing::Cubic, t));
 		m_pos.y += m_speed;
 		Vec2 pos = m_pos + Vec2{ m_deflection * Sin(t),0.0 };
-		Circle(pos, r).draw(Color(color).setAlpha(t > 8.0 ? 100 - (t - 8.0) * 50 : 100));
+		HSV hsv = HSV(color);
+		hsv.h = m_h;
+		Circle(pos, r).draw(hsv.toColor(t > 8.0 ? 100 - (t - 8.0) * 50 : 100));
 		return m_pos.y + r >= 0;
 	}
 };
