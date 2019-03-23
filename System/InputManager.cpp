aff22fca9335852611f653016625207be1a5ac93
mahou <tyanmahou@gmail.com>
2019-03-23 17:00:54 +0900

    add input manager

---
+++ System/InputManager.cpp
@@ -0,0 +1,97 @@
+﻿#include<Siv3D/Key.hpp>
+#include"InputManager.hpp"
+#include"PlayKey.h"
+class InputManager::Impl
+{
+private:
+	bool m_isRedClicked = false;
+	bool m_isBlueClicked = false;
+	bool m_isYellowClicked = false;
+public:
+	void update()
+	{
+		m_isRedClicked = m_isBlueClicked = m_isYellowClicked = false;
+
+		m_isRedClicked = PlayKey::Red().clicked;
+		m_isBlueClicked = PlayKey::Blue().clicked;
+		m_isYellowClicked = PlayKey::Yellow().clicked;
+	}
+
+	bool isRedClicked() const
+	{
+		return m_isRedClicked;
+	}
+
+	void popRed()
+	{
+		m_isRedClicked = false;
+	}
+
+	bool isBlueClicked() const
+	{
+		return m_isRedClicked;
+	}
+
+	void popBlue()
+	{
+		m_isBlueClicked = false;
+	}
+
+	void popYellow()
+	{
+		m_isYellowClicked = false;
+	}
+
+	bool isYellowClicked() const
+	{
+		return m_isYellowClicked;
+	}
+
+	bool isAnyClicked() const
+	{
+		return m_isRedClicked || m_isBlueClicked || m_isYellowClicked;
+	}
+};
+InputManager::InputManager() :
+	m_pImpl(std::make_shared<Impl>())
+{}
+
+bool InputManager::IsRedClicked()
+{
+	return Instance()->m_pImpl->isRedClicked();
+}
+
+void InputManager::PopRed()
+{
+	Instance()->m_pImpl->popRed();
+}
+
+bool InputManager::IsBlueClicked()
+{
+	return Instance()->m_pImpl->isBlueClicked();
+}
+
+void InputManager::PopBlue()
+{
+	Instance()->m_pImpl->popBlue();
+}
+
+bool InputManager::IsYellowClicked()
+{
+	return Instance()->m_pImpl->isYellowClicked();
+}
+
+void InputManager::PopYellow()
+{
+	Instance()->m_pImpl->popYellow();
+}
+
+void InputManager::IsAnyClicked()
+{
+	Instance()->m_pImpl->isAnyClicked();
+}
+
+void InputManager::Update()
+{
+	Instance()->m_pImpl->update();
+}
---
+++ System/InputManager.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include"Singleton.h"
+#include<memory>
+
+using NoteType = int;
+
+class InputManager final : protected Singleton<InputManager>
+{
+private:
+	class Impl;
+	std::shared_ptr<Impl> m_pImpl;
+
+	InputManager();
+public:
+
+	static bool IsRedClicked();
+	static void PopRed();
+
+	static bool IsBlueClicked();
+	static void PopBlue();
+
+	static bool IsYellowClicked();
+	static void PopYellow();
+
+	static void IsAnyClicked();
+	static void Update();
+
+	friend class Singleton<InputManager>;
+};
