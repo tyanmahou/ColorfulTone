c06e9e5f34c2524bf46e6fc0d247e1eecb10dbca
tyanmahou <tyanmahou@gmail.com>
2023-11-02 01:11:48 +0900

    add Input

---
+++ System/core/Input/InputManager.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <utils/Singleton/DynamicSingleton.hpp>
+#include <memory>
+
+namespace ct
+{
+    class InputManager final : protected DynamicSingleton<InputManager>
+    {
+        friend class DynamicSingleton<InputManager>;
+    public:
+
+        static bool IsRedClicked();
+        static void PopRed();
+
+        static bool IsBlueClicked();
+        static void PopBlue();
+
+        static bool IsYellowClicked();
+        static void PopYellow();
+
+        static bool IsAnyClicked();
+        static void Update();
+    private:
+        InputManager();
+    private:
+        class Impl;
+        std::unique_ptr<Impl> m_pImpl;
+    };
+}
