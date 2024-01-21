29fa798d6fe7a0a5e4f996e65cfa94eb1427597e
tyanmahou <tyanmahou@gmail.com>
2024-01-21 23:39:31 +0900

    add HighSpeedDemo

---
+++ System/core/HighSpeed/HighSpeedDemo.cpp
@@ -0,0 +1,136 @@
+﻿#include <core/HighSpeed/HighSpeedDemo.hpp>
+#include <Useful.hpp>
+//#include "Constants.hpp"
+#include <core/Play/PlayStyle/PlayStyle.hpp>
+//
+//#include"Note.h"
+//#include"Bar.h"
+#include <Siv3D.hpp>
+
+namespace ct
+{
+	HighSpeedDemo::HighSpeedDemo() :
+		m_offset(-300, 0, Easing::Quad, 400),
+		m_bgRect(400 - 45, 0, 90, 500),
+		m_judgeCircle(400, 300, 40)
+	{
+		//***********************************************************
+			//wav取得
+		Wave wav;
+		//新wavのサンプリング数
+		const size_t sample = 44100 * 10 + wav.lengthSample();
+		//無音作成
+		auto sam = WaveSample(0, 0);
+		wav.reserve(sample);
+		//10秒かんのwav
+		wav.insert(wav.begin(), 44100 * 10, sam);
+		m_sound = Audio(wav);
+		//***********************************************************
+
+			//ノーツ作成
+		PlayStyle::Instance()->setStyle(Game::Config().m_styleType);
+
+		for (size_t i = 0; i < 10; ++i)
+			for (size_t j = 0; j < 2; ++j) {
+				auto count = NotesData::RESOLUTION * i + NotesData::RESOLUTION * j / 2;
+				m_objects.emplace_back(std::make_shared<Note>(
+					Game::Config().m_styleType == PlayStyleType::Default ? 5 : 2,
+					count, 1));
+			}
+		for (size_t i = 0; i < 10; ++i) {
+			m_objects.emplace_back(std::make_shared<Bar>(NotesData::RESOLUTION * i, 1));
+		}
+
+	}
+	HighSpeedDemo::~HighSpeedDemo()
+	{
+		//m_sound.stop();
+	}
+
+	namespace
+	{
+
+		void SpeedUpdate(const s3d::InputGroup& key, Stopwatch& sw, float& scrollRate, int32 value)
+		{
+			if (
+				key.down() ||
+				key.pressed() && sw.ms() >= 80) {
+				int32 tmp = scrollRate * 10;
+				tmp += value;
+				scrollRate = tmp / 10.0f;
+				sw.restart();
+			}
+		}
+
+	}
+	bool HighSpeedDemo::update(float& scrollRate)
+	{
+		bool isCtrlPressed = KeyControl.pressed();
+		if (!m_sound.isPlaying())
+			m_sound.play();
+
+		if (KeyControl.down()) {
+			if (!m_offset.isMoving()) {
+				m_offset.start();
+				m_stopwatch.start();
+			}
+		}
+		if (!isCtrlPressed) {
+			m_offset.reset();
+			m_stopwatch.reset();
+		}
+		if (m_offset.isMoving())
+			return isCtrlPressed;
+
+		if (isCtrlPressed) {
+			SpeedUpdate(PlayKey::Up(), m_stopwatch, scrollRate, 1);
+			SpeedUpdate(PlayKey::Down(), m_stopwatch, scrollRate, -1);
+			SpeedUpdate(PlayKey::Right(), m_stopwatch, scrollRate, 10);
+			SpeedUpdate(PlayKey::Left(), m_stopwatch, scrollRate, -10);
+
+		}
+		if (scrollRate < 0.1f) {
+			scrollRate = 0.1f;
+		}
+		return isCtrlPressed;
+	}
+
+	void HighSpeedDemo::drawDemoNotes(const SoundBar& bar, float scrollRate)const
+	{
+		// TODO マスク処理
+		//m_bgRect.draw(ColorF(0, 0.5));
+
+		//Graphics2D::SetStencilState(StencilState::Replace);
+		//Graphics2D::SetStencilValue(stencilNum);
+
+		//m_bgRect.draw();
+
+		//Graphics2D::SetStencilState(StencilState::Test(StencilFunc::Equal));
+
+
+		//PlayStyle::Instance()->drawJudgeLine();
+
+		//const auto b = bar(m_sound);
+
+		//const auto nowCount = NotesData::RESOLUTION * b.bar + NotesData::RESOLUTION * (b.f);
+
+		//for (auto it = m_objects.rbegin(); it != m_objects.rend(); ++it) {
+		//	if ((*it)->getCount() - nowCount > 0)
+		//		(*it)->draw(nowCount, scrollRate);
+		//}
+		//Graphics2D::SetStencilState(StencilState::Default);
+
+	}
+	void HighSpeedDemo::draw(const SoundBar& min, const SoundBar& max, float scrollRate)const
+	{
+		{
+			Transformer2D t2d(Mat3x2::Translate({ m_offset.easeInOut() - 350,0 }));
+			drawDemoNotes(min, scrollRate);
+		}
+		{
+			Transformer2D t2d(Mat3x2::Translate({ m_offset.easeInOut() - 250,0 }));
+			drawDemoNotes(max, scrollRate);
+		}
+
+	}
+}
---
+++ System/core/HighSpeed/HighSpeedDemo.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <Siv3D/Audio.hpp>
+#include <Siv3D/Array.hpp>
+#include <Siv3D/EasingAB.hpp>
+#include <Siv3D/Rect.hpp>
+#include <Siv3D/Circle.hpp>
+#include <Siv3D/Stopwatch.hpp>
+#include <memory>
+
+namespace ct
+{
+	class Object;
+	class SoundBar;
+
+	class HighSpeedDemo
+	{
+	private:
+		s3d::Array<std::shared_ptr<Object>> m_objects;//デモ用譜面データ
+
+		s3d::EasingAB<s3d::int32> m_offset;
+
+		s3d::Audio m_sound;
+
+		const s3d::Rect m_bgRect;
+		const s3d::Circle m_judgeCircle;
+		s3d::Stopwatch m_stopwatch;
+		void drawDemoNotes(const SoundBar& bar, float scrollRate) const;
+	public:
+		HighSpeedDemo();
+		~HighSpeedDemo();
+		bool update(float& scrollRate);
+		void draw(const SoundBar& min, const SoundBar& max, float scrollRate)const;
+	};
+}
