4e851fc00404c2c7f88d2504604685e69b951139
tyanmahou <tyanmahou@gmail.com>
2024-01-23 02:46:12 +0900

    add ShaderCompile

---
+++ ShaderCompile/Main.cpp
@@ -0,0 +1,17 @@
+﻿# include <Siv3D.hpp>
+
+void Main()
+{
+	Array<FilePath> paths = Dialog::OpenFiles({ FileFilter{  U"HLSL", { U"hlsl" }} }, FileSystem::CurrentDirectory(), U"コンパイルファイルPS");
+	for (const FilePath& path : paths)
+	{
+		FilePath outPath = path.replaced(U"hlsl", U"ps");
+		if (Platform::Windows::CompileHLSLToFile(path, outPath, ShaderStage::Pixel, U"PS")) {
+			Print << U"OK << " << path;
+		} else {
+			Print << U"Error << " << path;
+		}
+	}
+	while (System::Update()) {
+	}
+}
