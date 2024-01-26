576f5605e20e3e9e90975e0c5a7391c30159759c
tyanmahou <tyanmahou@gmail.com>
2024-01-27 04:21:14 +0900

    update Encode

--- ColorfulTone/Main.cpp
+++ ColorfulTone/Main.cpp
@@ -1,8 +1,50 @@
 ﻿#include <Siv3D.hpp> 
 #include "MainApp.hpp"
 
+#include <utils/Encode/EncodeUtil.hpp>
+
+void EncodeShiftJISToUTF8()
+{
+    for (auto p : FileSystem::DirectoryContents(U"Music")) {
+        if (!FileSystem::IsFile(p)) {
+            continue;
+        }
+        if (auto ext = FileSystem::Extension(p); !(ext == U"ini" || ext == U"csv")) {
+            continue;
+        }
+        ct::EncodeUtil::ConvertShiftJISToUTF8(p);
+    }
+    for (auto p : FileSystem::DirectoryContents(U"Sample")) {
+        if (!FileSystem::IsFile(p)) {
+            continue;
+        }
+        if (auto ext = FileSystem::Extension(p); !(ext == U"ini" || ext == U"csv")) {
+            continue;
+        }
+        ct::EncodeUtil::ConvertShiftJISToUTF8(p);
+    }
+    for (auto p : FileSystem::DirectoryContents(U"Course")) {
+        if (!FileSystem::IsFile(p)) {
+            continue;
+        }
+        if (auto ext = FileSystem::Extension(p); !(ext == U"ctc")) {
+            continue;
+        }
+        ct::EncodeUtil::ConvertShiftJISToUTF8(p);
+    }
+    for (auto p : FileSystem::DirectoryContents(U"CustomFolder")) {
+        if (!FileSystem::IsFile(p)) {
+            continue;
+        }
+        if (auto ext = FileSystem::Extension(p); !(ext == U"ctfolder")) {
+            continue;
+        }
+        ct::EncodeUtil::ConvertShiftJISToUTF8(p);
+    }
+}
 void Main()
 {
+    // EncodeShiftJISToUTF8();
     ct::MainApp app;
     app.run();
 }
---
+++ System/utils/Encode/EncodeUtil.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <Siv3D/String.hpp>
+
+namespace ct
+{
+    class EncodeUtil
+    {
+    public:
+        /// <summary>
+        /// Shift-JISか
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        static bool IsMaybeShiftJIS(s3d::FilePathView filePath);
+
+        static bool ConvertShiftJISToUTF8(s3d::FilePathView filePath);
+    };
+}
