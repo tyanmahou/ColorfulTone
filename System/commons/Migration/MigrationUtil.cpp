abd40e5fdf071c76418922bc80fb2b3c00093a70
tyanmahou <tyanmahou@gmail.com>
2024-01-29 00:10:17 +0900

    update migration util

---
+++ System/commons/Migration/MigrationUtil.cpp
@@ -0,0 +1,19 @@
+﻿#include <commons/Migration/MigrationUtil.hpp>
+#include <Siv3D.hpp>
+
+
+namespace ct
+{
+    void MigrationUtil::MoveScoreFolder()
+    {
+        if (FileSystem::Exists(U"Score/CourseScore")) {
+            FileSystem::Rename(U"Score/CourseScore", U"UserData/CourseScore");
+        }
+        if (FileSystem::Exists(U"Score")) {
+            FileSystem::Rename(U"Score", U"UserData/Score");
+        }
+        if (FileSystem::Exists(L"System")) {
+            FileSystem::Rename(U"System", U"UserData/System");
+        }
+    }
+}
--- System/commons/Migration/MigrationUtil.hpp
+++ System/commons/Migration/MigrationUtil.hpp
@@ -4,6 +4,7 @@ namespace ct
 {
     class MigrationUtil
     {
-
+    public:
+        static void MoveScoreFolder();
     };
 }
