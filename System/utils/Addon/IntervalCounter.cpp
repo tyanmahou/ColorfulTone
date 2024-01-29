9fd22e38b2f2d2ee2e13783e502f52e29d2dbb31
tyanmahou <tyanmahou@gmail.com>
2024-01-29 23:06:50 +0900

    add IntervalCounter

---
+++ System/utils/Addon/IntervalCounter.cpp
@@ -0,0 +1,33 @@
+﻿#include <utils/Addon/IntervalCounter.hpp>
+#include <Siv3D.hpp>
+#include "IntervalCounter.hpp"
+
+namespace ct
+{
+    IntervalCounter::IntervalCounter(const Fps& fps):
+        m_interval(fps.deltaTime()),
+        m_current(0)
+    {
+    }
+
+    bool IntervalCounter::update()
+    {
+        m_isUpdateFrame = false;
+        if (m_interval <= 0) {
+            ++m_count;
+            m_isUpdateFrame = true;
+        } else {
+            m_current += Scene::DeltaTime();
+            if (m_current >= m_interval) {
+                m_current = s3d::Fmod(m_current, m_interval);
+                ++m_count;
+            }
+            m_isUpdateFrame = true;
+        }
+        return true;
+    }
+    bool IntervalCounter::isUpdatedMod(s3d::uint64 mod) const
+    {
+        return m_isUpdateFrame && m_count % mod == 0;
+    }
+}
---
+++ System/utils/Addon/IntervalCounter.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <Siv3D/Addon.hpp>
+#include <utils/Fps/Fps.hpp>
+
+namespace ct
+{
+    class IntervalCounter : public s3d::IAddon
+    {
+    public:
+        IntervalCounter(const Fps& fps = 60_fps);
+    protected:
+        bool update() override;
+    private:
+        s3d::uint64 count() const
+        {
+            return m_count;
+        }
+        bool isUpdated() const
+        {
+            return m_isUpdateFrame;
+        }
+        bool isUpdatedMod(s3d::uint64 mod = 1) const;
+
+        template<class Pred>
+        bool isUpdatedWhen(Pred pred)
+        {
+            return m_isUpdateFrame && pred(m_count);
+        }
+    private:
+        s3d::uint64 m_count = 0;
+        double m_interval = 0;
+        double m_current = 0;
+        bool m_isUpdateFrame = false;
+    };
+}
