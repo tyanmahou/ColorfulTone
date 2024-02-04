db95493497612ca62f8f5ab83c6008a6b8a71e20
tyanmahou <tyanmahou@gmail.com>
2024-02-04 23:53:56 +0900

    update NotesViewer

---
+++ NotesViewer/App/link.bat
@@ -0,0 +1,4 @@
+﻿mklink /d Music ..\..\ColorfulTone\App\Music
+mklink /d Resource ..\..\ColorfulTone\App\Resource
+mklink /d Shaders ..\..\ColorfulTone\App\Shaders
+mklink /d TapSE ..\..\ColorfulTone\App\TapSE
