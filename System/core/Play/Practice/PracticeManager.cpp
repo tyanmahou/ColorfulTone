7b0d84c365d5804c86a5467d1a5c9028e61f24b1
tyanmahou <tyanmahou@gmail.com>
2024-03-09 02:49:35 +0900

    add PracticeManagaer

---
+++ System/core/Play/Practice/PracticeManager.cpp
@@ -0,0 +1,18 @@
+﻿#include <core/Play/Practice/PracticeManager.hpp>
+
+namespace ct
+{
+    void PracticeManager::ChangePlayMode()
+    {
+        bool& m_isPractice = Instance()->m_isPractice;
+        m_isPractice = !m_isPractice;
+    }
+    void PracticeManager::SetPracticeMode(bool isPractice)
+    {
+        Instance()->m_isPractice = isPractice;
+    }
+    bool PracticeManager::IsPractice()
+    {
+        return Instance()->m_isPractice;
+    }
+}
---
+++ System/core/Play/Practice/PracticeManager.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <utils/Singleton/Singleton.hpp>
+
+namespace ct
+{
+    /// <summary>
+    /// 練習マネージャー
+    /// </summary>
+    class PracticeManager final : Singleton<PracticeManager>
+    {
+        friend class Singleton<PracticeManager>;
+    public:
+        static void ChangePlayMode();
+        static void SetPracticeMode(bool isPractice);
+        static bool IsPractice();
+    private:
+        bool m_isPractice = false;
+    };
+}
