02a9dd844e1108c8d6e747ed30edb7b0f3c109a6
tyanmahou <tyanmahou@gmail.com>
2024-03-10 04:01:59 +0900

    add PlayContext

---
+++ System/core/Play/Context/PlayContext.cpp
@@ -0,0 +1,29 @@
+﻿#include <core/Play/Context/PlayContext.hpp>
+
+namespace ct
+{
+    void PlayContext::ChangeAutoPlay()
+    {
+        SetAutoPlay(!IsAutoPlay());
+    }
+    void PlayContext::SetAutoPlay(bool isAuto)
+    {
+        Instance()->m_isAutoPlay = isAuto;
+    }
+    bool PlayContext::IsAutoPlay()
+    {
+        return Instance()->m_isAutoPlay;
+    }
+    void PlayContext::ChangePracticePlay()
+    {
+        SetPracticePlay(!IsPracticePlay());
+    }
+    void PlayContext::SetPracticePlay(bool isPractice)
+    {
+        Instance()->m_isPractice = isPractice;
+    }
+    bool PlayContext::IsPracticePlay()
+    {
+        return Instance()->m_isPractice;
+    }
+}
---
+++ System/core/Play/Context/PlayContext.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <utils/Singleton/Singleton.hpp>
+
+namespace ct
+{
+    /// <summary>
+    /// コンテキスト
+    /// </summary>
+    class PlayContext final : Singleton<PlayContext>
+    {
+        friend class Singleton<PlayContext>;
+    public:
+        // オートプレイ
+        static void ChangeAutoPlay();
+        static void SetAutoPlay(bool isAuto);
+        static bool IsAutoPlay();
+
+        // 練習モード
+        static void ChangePracticePlay();
+        static void SetPracticePlay(bool isPractice);
+        static bool IsPracticePlay();
+
+    private:
+        bool m_isAutoPlay = false;
+        bool m_isPractice = false;
+    };
+}
