8dbb313e148a44054c8c2757196c3cca61d477ef
tyanmahou <tyanmahou@gmail.com>
2024-03-31 16:02:54 +0900

    add IGameInputPlayer

---
+++ System/core/Input/IGameInputPlayer.hpp
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <core/Types.hpp>
+
+namespace ct
+{
+    enum class GameInputKind
+    {
+        Red,
+        Blue,
+        Yellow,
+    };
+
+    /// <summary>
+    /// 入力抽象クラス
+    /// </summary>
+    class IGameInputPlayer
+    {
+    public:
+        virtual ~IGameInputPlayer() = default;
+
+        /// <summary>
+        /// クリック判定
+        /// </summary>
+        /// <param name="kind"></param>
+        /// <returns></returns>
+        virtual bool isClicked(GameInputKind kind) = 0;
+
+        /// <summary>
+        /// Pressed判定
+        /// </summary>
+        /// <param name="kind"></param>
+        /// <returns></returns>
+        virtual bool isPressed(GameInputKind kind) = 0;
+
+        /// <summary>
+        /// クリックオフセット
+        /// </summary>
+        /// <param name="kind"></param>
+        /// <returns></returns>
+        virtual s3d::int64 clickedTimeOffset(GameInputKind kind) = 0;
+
+        /// <summary>
+        /// 強制入力
+        /// </summary>
+        /// <param name="kind"></param>
+        virtual void forceInput(GameInputKind kind) = 0;
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        virtual void update() = 0;
+    };
+}
